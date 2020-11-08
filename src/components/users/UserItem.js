29e34e32fbcb2d26ff0ff7566ef1a788ac1ddaf3
Shelton Gayles <sgbythelake@gmail.com>
2020-11-07 22:34:57 -0800

    converted navbar and useritem to functions

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,43 +1,27 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 
-class UserItem extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     id: 'id',
-  //     login: 'mojombo',
-  //     avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-  //     html_url: 'https://github.com/mojombo',
-  //   };
-  // }
-
-  // state = {
-  //   id: 'id',
-  //   login: 'mojombo',
-  //   avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-  //   html_url: 'https://github.com/mojombo',
-  // };
-
-  render() {
-    // const { login, avatar_url, html_url } = this.state;
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
