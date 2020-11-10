ec1d1ed920c73bb3fbb88254d270254c90872aa1
Shelton Gayles <sgbythelake@gmail.com>
2020-11-09 21:34:14 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // search github users
@@ -26,6 +28,12 @@ class App extends Component {
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   };
+  // Set Alert
+  setAlert = (msg, type) => {
+    // this.setState({ msg: msg, type: type });
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
     const { users, loading } = this.state;
@@ -33,10 +41,12 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
