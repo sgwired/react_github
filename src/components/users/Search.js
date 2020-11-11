323e6aec898ac5a7d64dfff57f067c9186bfd249
Shelton Gayles <sgbythelake@gmail.com>
2020-11-10 19:06:47 -0800

    convert components to functional

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
