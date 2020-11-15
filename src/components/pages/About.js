1f05aee4008c6ab8efc0e8c6d2403bb4bff4e5ce
Shelton Gayles <sgbythelake@gmail.com>
2020-11-14 18:10:25 -0800

    added home not found

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const About = () => {
   return (
-    <div>
+    <Fragment>
       <h1>About This App</h1>
       <p>App to search Github users</p>
       <p>Version: 1.0.0</p>
-    </div>
+    </Fragment>
   );
 };
 
